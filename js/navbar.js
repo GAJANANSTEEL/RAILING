115ab7b09574e72ebb94ac23ffdb7ca6fc1ce60b
GAJANANSTEEL <vickymaanzu@gmail.com>
2024-10-18 02:02:46 +0530

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -11,3 +11,12 @@ function navToggle() {
     navBar.removeAttribute("style");
   }
 }
+let index = 0;
+
+function showNextSlide() {
+    const slides = document.querySelector('.slider-container');
+    index = (index + 1) % slides.children.length; // Cycle through slides
+    slides.style.transform = `translateX(-${index * 100}%)`;
+}
+
+setInterval(showNextSlide, 3000); // Change slide every 3 seconds
